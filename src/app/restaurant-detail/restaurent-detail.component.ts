9ab5bbd64f47ad204af4aaea5ae9342d65df83b4
Israel gomes da lapa <israelgoomes@MacBook-Pro-de-israel.local>
2019-12-19 18:50:44 -0300

    web service, criacao do detalhe e menu

---
+++ src/app/restaurant-detail/restaurent-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurent-detail',
+  templateUrl: './restaurent-detail.component.html',
+  styleUrls: ['./restaurent-detail.component.css']
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
